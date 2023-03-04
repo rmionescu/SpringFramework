aa229d11af88e25c960f9aef111d7db22159ade6
rmionescu <rmionescu@yahoo.com>
2023-03-04 12:10:56 +0200

    Added Publisher POJO

---
+++ eclipse-workspace/spring5webapp/spring5webapp/src/main/java/emiione/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,9 @@
+package emiione.springframework.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import emiione.springframework.spring5webapp.domain.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
